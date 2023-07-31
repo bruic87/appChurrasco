39ffbcd117516d6b717ae5d4a78e04438c116e75
bruic87 <bruic@outlook.com>
2023-07-31 08:53:15 -0300

    'estilizacao de app single page'

---
+++ appChurrasco/componentes/Texto/estilo.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+  const estilo = StyleSheet.create({
+    boxTexto: {
+      padding: 30
+    },
+
+    boxTextoTitulo: {
+      textAlign: "center",
+      fontSize: 20,
+      fontWeight: '700',
+      color: '#313131',
+      marginBottom: 20
+    },
+
+    boxTextoSubtitulo: {
+      textAlign: "justify",
+      color: '#313131',
+      lineHeight: 20
+    },
+  });
+
+  export default estilo;
